08b43f0e22f15b7cafb8e7b505cf156ef611f888
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2021-06-12 18:38:19 -0400

    Change db settings

--- hackgirlsummer/settings.py
+++ hackgirlsummer/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'hackgirlsummer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spotdb',
+        'USER': 'postgres',
+        'PASSWORD': 'atd123389',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
